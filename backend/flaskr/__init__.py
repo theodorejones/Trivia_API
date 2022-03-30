105d5129636aa70b4a101fb88986331c7235301e
theodorejones <70782338+theodorejones@users.noreply.github.com>
2022-03-30 14:55:22 -0400

    Update __init__.py

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -9,15 +9,12 @@ from models import setup_db, Question, Category
 QUESTIONS_PER_PAGE = 10
 
 def paginate_questions(request, selection):
-    page = request.args.get('page', 1, type=int)
     start = (page - 1) * QUESTIONS_PER_PAGE
     end = start + QUESTIONS_PER_PAGE
-
+    page = request.args.get('page', 1, type=int)
     questions = [question.format() for question in selection]
     current_questions = questions[start:end]
-
     return current_questions
-
 
 def create_app(test_config=None):
     # create and configure the app
